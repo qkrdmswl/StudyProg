2416564e97c940bfda14c7d1a973945574473a26
qkrdmswl <eunji303@naver.com>
2021-01-29 21:07:00 +0900

    web

---
+++ [Web스터디]2주차_responsive_header/week2.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    const toggleBtn = document.querySelector('.navbar__toggleBtn');
+    const menu = document.querySelector('.navbar__menu');
+    
+    toggleBtn.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    });
+}
